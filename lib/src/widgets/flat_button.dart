6ac0ab92b87e8efc671a4159ed6b613efb3e6011
Leon Wong <ybbwk@vip.qq.com>
2021-10-13 14:48:48 +0800

    将FLFlatButton置为过时

--- lib/src/widgets/flat_button.dart
+++ lib/src/widgets/flat_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flui/src/common/defines.dart';
 
+@deprecated
 class FLFlatButton extends StatelessWidget {
   const FLFlatButton(
       {Key? key,
